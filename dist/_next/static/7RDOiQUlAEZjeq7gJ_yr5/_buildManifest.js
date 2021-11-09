eef288453aff4ab9b757629539f147f4da58621a
Jhu-Ning Chang <zhuning331@gmail.com>
2021-11-09 14:15:33 +0800

    add next export files

---
+++ dist/_next/static/7RDOiQUlAEZjeq7gJ_yr5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/456-baccce9dd825e441.js","static/chunks/pages/index-fd7a600917a936ff.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
